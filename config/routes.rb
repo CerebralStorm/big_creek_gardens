fd39ea3e20c051414f059ceed800b27596dc621b
Cody Tanner <ctanner@instructure.com>
2018-08-17 14:10:37 -0600

    adds products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :products
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'static#index'
