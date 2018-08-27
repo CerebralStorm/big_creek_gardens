b0971c255f2fce99e8e3457cdb558dcb28d47949
Cody Tanner <ctanner@instructure.com>
2018-08-27 14:55:52 -0600

    Meta tags

--- app/javascript/components/shop.jsx
+++ app/javascript/components/shop.jsx
@@ -5,8 +5,27 @@ import PropTypes from 'prop-types'
 class Shop extends React.Component {
   render() {
     return (
-      <div>
-        <h1>SHOP</h1>
+      <div className="feature">
+        <div className="container">
+          <div className="text-center row">
+            <div className="col-md-3">
+              <h2>Full Responsive</h2>
+              <p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+            </div>
+            <div className="col-md-3">
+              <h2>Retina Ready</h2>
+              <p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+            </div>
+            <div className="col-md-3">
+              <h2>Full Responsive</h2>
+              <p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+            </div>
+            <div className="col-md-3">
+              <h2>Friendly Code</h2>
+              <p>Quisque eu ante at tortor imperdiet gravida nec sed turpis phasellus.</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
