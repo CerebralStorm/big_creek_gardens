9c35f0a4ed831a8277666977b2a4f0c9cee1a4ae
Cody Tanner <ctanner@instructure.com>
2018-08-27 21:08:30 -0600

    rubocop

--- spec/factories/charges.rb
+++ spec/factories/charges.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :charge do
     user_id 1
     amount 1.5
-    currency "MyString"
-    description "MyString"
+    currency 'MyString'
+    description 'MyString'
   end
 end
