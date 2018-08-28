9a6ff0b33682c19c590c22573b7c82caedcacf27
Cody Tanner <ctanner@instructure.com>
2018-08-27 21:07:48 -0600

    Adds charges

--- db/migrate/20180828030134_create_charges.rb
+++ db/migrate/20180828030134_create_charges.rb
@@ -1,7 +1,7 @@
 class CreateCharges < ActiveRecord::Migration[5.2]
   def change
     create_table :charges do |t|
-      t.integer :user_id
+      t.integer :user_id, index: true
       t.float :amount
       t.string :currency
       t.string :description
