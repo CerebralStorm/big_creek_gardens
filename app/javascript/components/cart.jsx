b5bdc9cc7674ae29b35099e2c5958db95fdd2017
Cody Tanner <ctanner@instructure.com>
2018-08-28 21:50:36 -0600

    Styles the cart

--- app/javascript/components/cart.jsx
+++ app/javascript/components/cart.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { loadCart } from '../actions/cart'
+import { Link } from 'react-router-dom'
 
 import CartItem from './cart/cart_item'
 import CheckoutForm from './cart/checkout_form'
@@ -15,18 +16,44 @@ class Cart extends React.Component {
 
   render() {
     return (
-      <div>
-        {Object.values(this.props.cart).map(cartItem => (
-          <CartItem key={cartItem.product.id} cartItem={cartItem} />
-        ))}
-        <StripeProvider apiKey={ENV.stripe_key} >
-          <div className="example">
-            <h1>Checkout</h1>
-            <Elements>
-              <CheckoutForm />
-            </Elements>
+      <div className="container cart-container">
+        <div className="card shopping-cart">
+          <div className="card-header bg-dark text-light">
+            <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            Shipping cart
+            <Link to={'/shop'} className="btn btn-outline-success btn-sm pull-right">
+              Continue Shopping
+            </Link>
+            <div className="clearfix"></div>
           </div>
-        </StripeProvider>
+          <div class="card-body">
+            {Object.values(this.props.cart).map(cartItem => (
+              <div>
+                <CartItem key={cartItem.product.id} cartItem={cartItem} />
+                <hr />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div class="card-footer">
+          <div class="coupon col-md-5 col-sm-5 no-padding-left pull-left">
+            <div class="row">
+              <div class="col-6">
+                <input type="text" class="form-control" placeholder="coupon code" />
+              </div>
+              <div class="col-6">
+                <input type="submit" class="btn btn-default" value="Use coupon" />
+              </div>
+            </div>
+          </div>
+          <div class="pull-right" style={{margin: '10px'}}>
+            <a href="" class="btn btn-success pull-right">Checkout</a>
+            <div class="pull-right total-price" style={{margin: '5px'}}>
+              <strong>Total price: <b>$50.00</b></strong>
+            </div>
+          </div>
+          <div className='clearfix'></div>
+        </div>
       </div>
     );
   }
--- app/javascript/components/cart/cart_item.jsx
+++ app/javascript/components/cart/cart_item.jsx
@@ -11,13 +11,31 @@ class CartItem extends React.Component {
   }
 
   render() {
-    console.log(this.props.cartItem)
     return (
-      <div className="product-tile">
-        <div className="product-details">
-          <h6>{this.props.cartItem.product.name}</h6>
-          <p className="product-tile-description">{this.props.cartItem.description}</p>
-          <p>{this.props.cartItem.product.price}</p>
+      <div className="row">
+        <div className="col-12 col-sm-12 col-md-2 text-center">
+          <img className="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80" />
+        </div>
+        <div className="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
+          <h4 className="product-name"><strong>{this.props.cartItem.product.name}</strong></h4>
+          <h4><small>{this.props.cartItem.product.description}</small></h4>
+        </div>
+        <div className="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
+          <div className="col-3 col-sm-3 col-md-6 text-md-right line-item-price">
+            <h6><strong>{this.props.cartItem.product.price} <span className="text-muted">x</span></strong></h6>
+          </div>
+          <div className="col-4 col-sm-4 col-md-4">
+            <div className="quantity">
+              <input type="button" value="+" className="plus" />
+              <input type="number" step="1" max="99" min="1" value="1" title="Qty" className="qty" size="4" />
+              <input type="button" value="-" className="minus" />
+            </div>
+          </div>
+          <div className="col-2 col-sm-2 col-md-2 text-right">
+            <button type="button" className="btn btn-outline-danger btn-xs">
+              <i className="fa fa-trash" aria-hidden="true"></i>
+            </button>
+          </div>
         </div>
       </div>
     );
