51c234c06d8ab3cb22b03c0c4e2e7daf2ce99fc0
Cody Tanner <ctanner@instructure.com>
2018-09-13 14:03:28 -0600

    product controller spec

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -2,31 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ProductsController, type: :controller do
   render_views
-  let(:valid_session) { {} }
-  let!(:product) { FactoryBot.create(:product) }
-
   before do
     request.env['CONTENT_TYPE'] = 'application/json'
+    request.env['HTTP_ACCEPT'] = 'application/json'
+  end
+
+  def product_json(product)
+    {
+      'id' => product.id,
+      'name' => product.name,
+      'description' => product.description,
+      'price' => product.price,
+      'image_url' => product.image.url
+    }
   end
 
   describe 'GET #index' do
-    let!(:result) do
+    let!(:product) { FactoryBot.create(:product) }
+    let(:expected_response) do
       {
-        products: [
-          {
-            id: product.id,
-            name: product.name,
-            description: product.description,
-            price: product.price,
-            image_url: nil
-          }
+        'products' => [
+          product_json(product)
         ]
       }
     end
 
-    it 'returns products json' do
-      get :index, params: {}, format: :json
-      expect(response.body).to eq result.to_json
+    it 'returns the products for the current_user' do
+      post :index, params: {}
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).to eq JSON.parse(expected_response.to_json)
     end
   end
 end
